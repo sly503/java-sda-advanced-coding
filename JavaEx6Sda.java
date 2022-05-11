73739546d05a975e89a84f04b285d4ad9049b4e1
sly503 <fatjonrami@gmail.com>
2022-05-11 22:44:10 +0200

    ex7

---
+++ JavaEx6Sda.java
@@ -0,0 +1,23 @@
+
+import java.util.TreeMap;
+
+//Create a method that accepts TreeMap and prints the first and last EntrySet in the console.
+public class JavaEx6Sda {
+
+    public static void main(String[] args) {
+        TreeMap<String, String> map = new TreeMap<>();
+        map.put("A", "1");
+        map.put("B", "2");
+        map.put("C", "3");
+        printFirstAndLastEntrySet(map);
+        
+    }
+
+    public static void printFirstAndLastEntrySet(TreeMap<String, String> map) {
+
+        System.out.println(map.firstEntry());
+        System.out.println(map.lastEntry());
+
+    }
+
+}
---
+++ JavaEx7Sda/Magazine.java
@@ -0,0 +1,42 @@
+package JavaEx7Sda;
+
+public class Magazine {
+
+
+    private int capacity;
+    private int bullets;
+
+
+    public Magazine(int i) {
+        this.capacity = i;
+        this.bullets = 0;
+
+    }
+
+    public void loadBullet(String bullet) {
+        if (bullets < capacity) {
+            bullets++;
+        } else {
+            System.out.println("Magazine is full");
+        }
+
+    }
+
+    public boolean isLoaded() {
+        if (bullets > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void shot() {
+        if (isLoaded()) {
+            System.out.println("Bang!");
+            bullets--;
+        } else {
+            System.out.println("Empty magazine");
+        }
+    }
+
+}
